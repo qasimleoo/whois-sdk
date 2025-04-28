9246d7de43f0855f1802e98e79b463d707537238
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-04-28 12:07:10 +0000

    Release 1.0.15

--- src/whoisfreaks/whois/types/ip_whois_response.py
+++ src/whoisfreaks/whois/types/ip_whois_response.py
@@ -7,10 +7,11 @@ from ...core.pydantic_utilities import IS_PYDANTIC_V2, UniversalBaseModel
 from .inet_num import InetNum
 from .ip_contact import IpContact
 from .organization import Organization
+from .status import Status
 
 
 class IpWhoisResponse(UniversalBaseModel):
-    status: typing.Optional[bool] = None
+    status: typing.Optional[Status] = None
     ip_address: typing.Optional[str] = None
     query_time: typing.Optional[str] = None
     whois_server: typing.Optional[str] = None
@@ -19,6 +20,10 @@ class IpWhoisResponse(UniversalBaseModel):
     technical_contacts: typing.Optional[typing.List[IpContact]] = None
     abuse_contacts: typing.Optional[typing.List[IpContact]] = None
     whois_raw_response: typing.Optional[str] = None
+    timestamp: typing.Optional[str] = None
+    error: typing.Optional[str] = None
+    message: typing.Optional[str] = None
+    path: typing.Optional[str] = None
 
     if IS_PYDANTIC_V2:
         model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="allow", frozen=True)  # type: ignore # Pydantic v2
